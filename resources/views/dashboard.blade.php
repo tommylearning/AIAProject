c3a3993cee0cb4d46babad5bbc13b64a818d83a9
tommyliew <tommyliew61@gmail.com>
2021-07-17 18:13:26 +0800

    dashboard initial setup

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,10 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>dashboard</title>
+    <link href="{{mix('css/app.css')}}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
-        <dashboard-component></dashboard-component>
+        <dashboard-component
+            :contact-details="{{json_encode($contactDetails)}}"
+        ></dashboard-component>
     </div>
     <script src="{{ mix('js/app.js') }}"></script>
 </body>
